f3b2e2caf2e61d958b398fd691e73fe75ea23770
hp <hp@LAPTOP-RNVJM311>
2022-10-02 15:55:23 +0530

    secound commit

---
+++ Day_2datatypes/src/com/tnsindia/datatype/DemoOnDataType.java
@@ -0,0 +1,15 @@
+package com.tnsindia.datatype;
+import java.util.Scanner;
+public class DemoOnDataType {
+
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter a number");
+		int  a=sc.nextInt();
+		System.out.println(a);
+		
+
+	}
+
+}
