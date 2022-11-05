949f906b2bf767feb884d5ac80285aaaf4d1e854
hp <hp@LAPTOP-RNVJM311>
2022-11-06 02:29:38 +0530

    commit eleven

---
+++ Day11-Collections/src/com/org/tns/indiacomparatordemo/Employee.java
@@ -0,0 +1,23 @@
+package com.org.tns.indiacomparatordemo;
+
+public class Employee {
+	private int age;
+	private String name;
+	//setters and getters method
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
+
+
